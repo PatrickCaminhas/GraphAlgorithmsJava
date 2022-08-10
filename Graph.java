56b41bb57c002390ed358d9b2ad051536211a5fa
Patrick Caminhas <42319923+PatrickCaminhas@users.noreply.github.com>
2022-08-10 23:31:02 +0000

    aula 2

--- Graph.java
+++ Graph.java
@@ -1,60 +1,101 @@
 import java.util.Arrays;
 
 public class Graph {
-  private int countNodes;
-  private int countEdges;
-  private int[][] adjMatrix;
+    private int countNodes;
+    private int countEdges;
+    private int [][] adjMatrix;
 
-  public Graph(int countNodes) {
-    this.countNodes = countNodes;
-    this.adjMatrix = new int[countNodes][countNodes];
-  }
 
-  public int getCountNodes() {
-    return countNodes;
-  }
 
-  public int getCountEdges() {
-    return countEdges;
-  }
-
-  public int[][] getAdjMatrix() {
-    return adjMatrix;
-  }
-
-  @Override
-  public String toString() {
-    String str = "";
-    for (int i = 0; i < this.adjMatrix.length; i++) {
-      for (int j = 0; j < this.adjMatrix[i].length; j++) {
-        str += this.adjMatrix[i][j] + "\t";
-      }
-      str += "\n";
-    }
-    return str;
-  }
-
-  public void addEdge(int source, int sink, int weight) {
-    if (source < 0 || source > this.countNodes - 1 || sink < 0 || sink > this.countNodes - 1 || weight <= 0) {
-      System.err.println("Invalid edge: " + source + " " + sink + " " + weight);
-      return;
-    }
-    this.adjMatrix[source][sink] = weight;
-    this.countEdges++;
-  }
-
-  public int degree(int node) {
-    // returns the degree of a node
-    if (node < 0 || node > this.countNodes - 1) {
-      System.err.println("Invalid node: " + node);
-    }
-    int degree = 0;
-    for (int j = 0; j < this.adjMatrix[node].length; j++) {
-      if (this.adjMatrix[node][j] != 0) {
-        ++degree;
-      }
+    public Graph(int countNodes){
+        this.countNodes = countNodes;
+        this.adjMatrix = new int[countNodes][countNodes];
     }
 
-    return degree;
-  }
+    public int getCountNodes() {
+        return countNodes;
+    }
+
+    public int getCountEdges() {
+        return countEdges;
+    }
+
+    public int[][] getAdjMatrix() {
+        return adjMatrix;
+    }
+
+    @Override
+    public String toString() {
+        String str  = "";
+        for(int i = 0; i < this.adjMatrix.length; i++){
+            for(int j = 0; j < this.adjMatrix[i].length; j++){
+                str+= this.adjMatrix[i][j] + "\t";
+            }
+            str += "\n";
+        }
+        return str;
+    }
+
+
+    public void addEdge(int source, int sink, int weight){
+        if(source < 0 || source > this.countNodes - 1 || sink < 0 || sink >  this.countNodes - 1 || weight <= 0) {
+                System.err.println("Invalid edge: " + source +" "+ sink +" "+ weight);
+                return;
+        }
+            this.adjMatrix[source][sink]=weight;
+             this.countEdges++;
+    }
+
+    public int degree (int node){
+        //returns the degree of a node
+        if(node < 0 || node > this.countNodes -1){
+            System.err.println("Invalid node: " +  node);
+        }
+        int degree = 0;
+        for(int j = 0; j < this.adjMatrix[node].length; j++){
+            if(this.adjMatrix[node][j]!=0){
+                ++degree;
+            }
+        }
+
+        return degree;
+    }
+
+    public int highestDegree(){
+        int aux=0;
+        int high=0;
+        for(int i=0; i< this.adjMatrix.length; i++){
+            aux=this.degree(i);
+            if(aux>high){
+                high=aux;
+            }
+        }
+        return high;
+    }
+
+
+    public int lowestDegree(){
+        int aux=0;
+        int low=this.adjMatrix.length;
+        for(int i=0; i< this.adjMatrix.length; i++){
+            aux=this.degree(i);
+            if(aux<low){
+                low=aux;
+            }
+        }
+        return low;
+    }
+
+    public Graph complement (){
+        Graph complent = new Graph(this.adjMatrix.length);
+        for(int i = 0; i< adjMatrix.length; i++){
+            for(int j=0; j<adjMatrix[i].length; j++){
+                if(adjMatrix[i][j]==0 && i!=j)
+                {
+                    complent.addEdge(i,j,1);
+                }
+            }
+        }
+        return complent;
+    }
 }
--- Main.java
+++ Main.java
@@ -2,15 +2,18 @@ public class Main {
   public static void main(String[] args) {
 
     Graph g1 = new Graph(4);
-    g1.addEdge(0, 1, 3);
-    g1.addEdge(1, 0, 3);
-    g1.addEdge(0, 3, 4);
-    g1.addEdge(3, 0, 4);
-    g1.addEdge(3, 4, 2); // AVISO
+    g1.addEdge(0, 1, 1);
+    g1.addEdge(1, 0, 1);
+    g1.addEdge(0, 3, 1);
+    g1.addEdge(3, 0, 1);
+    g1.addEdge(3, 4, 1); // AVISO
     System.out.println(g1);
-    g1.degree(0);
-    g1.degree(1);
-    g1.degree(2);
-    g1.degree(3);
+    System.out.println(g1.degree(0));
+    System.out.println(g1.degree(1));
+    System.out.println(g1.degree(2));
+    System.out.println(g1.degree(3));
+    System.out.println("MAIOR GRAU: " + g1.highestDegree());
+    System.out.println("MENOR GRAU: " + g1.lowestDegree());
+    System.out.println(g1.complement());
   }
 }
